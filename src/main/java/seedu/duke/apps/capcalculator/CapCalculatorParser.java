b67a9dd7658a43f5511a4c45b445e2883eec159f
JuZihao <57383789+JuZihao@users.noreply.github.com>
2020-10-23 21:27:10 +0800

    Added more testing

--- src/main/java/seedu/duke/apps/capcalculator/CapCalculatorParser.java
+++ src/main/java/seedu/duke/apps/capcalculator/CapCalculatorParser.java
@@ -2,6 +2,7 @@ package seedu.duke.apps.capcalculator;
 
 import seedu.duke.apps.capcalculator.commands.CurrentCommand;
 import seedu.duke.apps.capcalculator.commands.SetTargetCommand;
+import seedu.duke.apps.capcalculator.exceptions.CapCalculatorException;
 import seedu.duke.global.exceptions.CommandParserException;
 import seedu.duke.global.Command;
 import seedu.duke.global.objects.Person;
@@ -13,6 +14,9 @@ import java.util.Scanner;
  * Class representing the parser used in the CAP Calculator app.
  */
 public class CapCalculatorParser {
+    private static final String PROMPT_SU_CHOICES = "Enter the number corresponding to the method you wish to S/U:\n"
+            + "\t1) Semester\n"
+            + "\t2) Modules";
     private static final String INVALID_COMMAND_MESSAGE = "OOPS!!! I'm sorry, but I don't know what that means :-(";
     private static final String CURRENT_COMMAND = "current";
     private static final String SET_TARGET_COMMAND = "set target";
@@ -39,9 +43,18 @@ public class CapCalculatorParser {
         } else if (userInput.equals(EXIT_COMMAND)) {
             return new Command(true);
         } else if (userInput.equals((SET_SU_COMMAND))) {
-            return SetSuParser.parse(currentPerson, in);
+            promptUserForSuCommand();
+            String choice = in.nextLine().trim();
+            return SetSuParser.parse(choice, currentPerson, in);
         } else {
             throw new CommandParserException(INVALID_COMMAND_MESSAGE);
         }
+    }
+
+    /**
+     * Prompt user to enter which S/U command to be parsed.
+     */
+    private static void promptUserForSuCommand() {
+        System.out.println(PROMPT_SU_CHOICES);
     }
 }
---
+++ src/test/java/seedu/duke/apps/capcalculator/SetSuParserTest.java
@@ -0,0 +1,66 @@
+package seedu.duke.apps.capcalculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.duke.apps.capcalculator.commands.SetSuByModuleCommand;
+import seedu.duke.apps.capcalculator.commands.SetSuBySemesterCommand;
+import seedu.duke.global.Command;
+import seedu.duke.global.objects.Person;
+import seedu.duke.ui.Ui;
+
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class SetSuParserTest {
+
+    private static final String INVALID_SU_ERROR = "Number entered does not correspond to any S/U method.";
+    private Person currentPerson;
+    private Scanner in;
+
+    @BeforeEach
+    public void setup() {
+        currentPerson = new Person("Bob");
+        in = new Ui().getScanner();
+    }
+
+    @Test
+    void parse_SetSuByModuleCommand_parsedCorrectly() {
+        final String choice = "2";
+        parseAndAssertCommandType(choice, SetSuByModuleCommand.class, currentPerson, in);
+    }
+
+    @Test
+    void parse_SetSuBySemesterCommand_parsedCorrectly() {
+        final String choice = "1";
+        parseAndAssertCommandType(choice, SetSuBySemesterCommand.class, currentPerson, in);
+    }
+
+    @Test
+    void parse_unknownChoice_exceptionThrown() {
+        try {
+            final String unknownChoice = "unknown choice";
+            SetSuParser.parse(unknownChoice, currentPerson, in);
+        } catch (Exception e) {
+            assertEquals(INVALID_SU_ERROR, e.getMessage());
+        }
+    }
+
+    /**
+     * Parses input and asserts the class/type of the returned command object.
+     *
+     * @param choice to be parsed
+     * @param expectedCommandClass expected class of returned command
+     */
+    private <T extends Command> void parseAndAssertCommandType(String choice, Class<T> expectedCommandClass,
+                                                               Person currentPerson, Scanner in) {
+        try {
+            final Command result = SetSuParser.parse(choice, currentPerson, in);
+            assertTrue(result.getClass().isAssignableFrom(expectedCommandClass));
+        } catch (Exception e) {
+            fail();
+        }
+    }
+}
