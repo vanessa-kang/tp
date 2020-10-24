b3d4249dd4a2e65d624e0ce20a360a522dfc2473
harryleecp <e0406315@u.nus.edu>
2020-10-24 17:25:05 +0800

    Added authorship for tests

--- src/test/java/seedu/duke/apps/academicplanner/commons/EditUtilsTest.java
+++ src/test/java/seedu/duke/apps/academicplanner/commons/EditUtilsTest.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+//@@author harryleecp
 class EditUtilsTest {
 
     ModuleLoader allModules;
